242e2ff42cbff65caaf4a8812415457c44e8743a
GeorgeChernyshov <ridiculousman@yandex.ru>
2020-02-19 22:56:47 +0300

    Create Helpers.cs

---
+++ Lab2_Chernyshov_Figures/Helpers/Helpers.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Helpers.HelpersExceptions;
+
+namespace Helpers
+{
+    /// <summary>
+    /// Класс, содержащий вспомогательные функции
+    /// </summary>
+    public static class Helpers
+    {
+        /// <summary>
+        /// Функция, выполняющая приведение строки к целому числу.
+        /// Перед приведением производится проверка строки на корректность
+        /// </summary>
+        /// <param name="input">Входная строка</param>
+        /// <returns>Целое число</returns>
+        public static int TryParseInt(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+                if (!(Char.IsDigit(input[i]) || (input[i] == '-' && i == 0)))
+                    throw new NotANumberException();
+
+            if (input[0] == '0' && input.Length > 1)
+                throw new NotANumberException();
+
+            int answer = 0;
+            if (!Int32.TryParse(input, out answer))
+                throw new NotANumberException();
+
+            return answer;
+        }
+
+        /// <summary>
+        /// Функция, выполняющая приведение строки к числу формата Double
+        /// Перед приведением производится проверка строки на корректность
+        /// </summary>
+        /// <param name="input">Входная строка</param>
+        /// <returns>Полученное число формата Double</returns>
+        public static double TryParseDouble(string input)
+        {
+            double answer = 0;
+            if (!Double.TryParse(input, out answer))
+                throw new NotANumberException();
+
+            return answer;
+        }
+    }
+}
