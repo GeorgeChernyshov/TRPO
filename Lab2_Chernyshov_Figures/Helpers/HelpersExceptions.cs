8413488859a7782d3727d59d9c0d459315d304c9
GeorgeChernyshov <ridiculousman@yandex.ru>
2020-02-19 22:58:16 +0300

    Add files via upload

---
+++ Lab2_Chernyshov_Figures/Helpers/HelpersExceptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    /// <summary>
+    /// Класс, содержащий исключения, выбрасываемые вспомогательными функциями
+    /// </summary>
+    public static class HelpersExceptions
+    {
+        /// <summary>
+        /// Исключение, бросаемое в случае, когда параметр не является числом
+        /// </summary>
+        public class NotANumberException : Exception
+        {
+            /// <summary>
+            /// Сообщение об ошибке
+            /// </summary>
+            const string message = "Введённое значение не является корректным числом.";
+
+            /// <summary>
+            /// Конструктор класса
+            /// </summary>
+            public NotANumberException() : base(message) { }
+        }
+
+        /// <summary>
+        /// Исключение, бросаемое в случае, когда параметр является отрицательным числом
+        /// </summary>
+        public class NegativeNumberException : Exception
+        {
+            /// <summary>
+            /// Сообщение об ошибке
+            /// </summary>
+            const string message = "Введено отрицательное число.";
+
+            /// <summary>
+            /// Конструктор класса
+            /// </summary>
+            public NegativeNumberException() : base(message) { }
+        }
+    }
+}
